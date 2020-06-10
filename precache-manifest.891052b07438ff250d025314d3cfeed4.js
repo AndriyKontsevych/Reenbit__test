fad35d71652c1ef07263837edd30881a09b0410b
Andriy <admin@Andriy.local>
2020-06-10 10:33:37 +0300

    Updates

---
+++ precache-manifest.891052b07438ff250d025314d3cfeed4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c0c8019a4faca452d1d5d7b9a63e942",
+    "url": "/Reenbit__test/index.html"
+  },
+  {
+    "revision": "7a44f31620dda5bd4c9e",
+    "url": "/Reenbit__test/static/css/main.8a4ab314.chunk.css"
+  },
+  {
+    "revision": "76cc1a91f49c83ffe49d",
+    "url": "/Reenbit__test/static/js/2.b0277120.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Reenbit__test/static/js/2.b0277120.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a44f31620dda5bd4c9e",
+    "url": "/Reenbit__test/static/js/main.6d62b325.chunk.js"
+  },
+  {
+    "revision": "77d400e9ff6bcda0148d",
+    "url": "/Reenbit__test/static/js/runtime-main.ff3de3ca.js"
+  }
+]);
